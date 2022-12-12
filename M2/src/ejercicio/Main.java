2a3b321f343f50007c94929497b8de6a270c5a22
mcosano <mcosano@106-03.info2>
2022-12-12 11:12:40 +0100

    Commit del alumno 2 con el primer mensaje

--- M2/src/ejercicio/Main.java
+++ M2/src/ejercicio/Main.java
@@ -3,7 +3,8 @@ package ejercicio;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hola, soy Manuel Cosano");
 
 	}
 
