f5b2a514cffaeaac4f618d8eafb0f03238d7d521
mditaranto <mditaranto@106-04.info2>
2022-12-13 09:43:41 +0100

    Alumno 1 mensaje de saludo

--- M2/src/ejercicio/Main.java
+++ M2/src/ejercicio/Main.java
@@ -3,8 +3,9 @@ package ejercicio;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Hola, soy Manuel Cosano");
+		System.out.println("Hola, soy Matias Ditaranto");
 		
-
 
 	}
 
